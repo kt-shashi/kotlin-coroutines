3435b995965d5696b8b47b784d05a177fa725bd1
shashiofficial <shashiofficial10@gmail.com>
2020-11-26 18:27:12 +0530

    Added async and await

--- app/src/main/java/com/shashi/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/shashi/kotlincoroutines/MainActivity.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +15,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         super.onCreate(savedInstanceState)
 
+        coroutineContext()
+
+        updateUiInMainThread()
+
+        runBlockingDemo()
+
+        jobCancel()
+
+        asyncAndAwait()
+    }
+
+    private fun coroutineContext() {
         //Dispatchers.Main -> For UI operations
         //Dispatchers.IO -> For Data operations(Networking/Database Handling)
         //Dispatchers.Default -> For Complex operations
@@ -30,9 +43,11 @@ class MainActivity : AppCompatActivity() {
             Log.d(TAG, "Answer1: $networkCallAnswer2")
         }
 
-        //Custom thread
+        //For Custom thread
         //GlobalScope.launch(newSingleThreadContext("CustomThreadName")) { }
+    }
 
+    private fun updateUiInMainThread() {
         //If you want to get data from Network and then update UI in Main thread
         GlobalScope.launch(Dispatchers.IO) {
             Log.d(TAG, "IO thread: ${Thread.currentThread().name}")
@@ -44,14 +59,19 @@ class MainActivity : AppCompatActivity() {
                 textViewMain.text = answer
             }
         }
+    }
 
+    private fun runBlockingDemo() {
         //If you want to call a suspend fun without coroutine behaviour, use runBlocking
         runBlocking {
             //This blocks the Main thread
             delay(1000L)
         }
+    }
 
-        val job = GlobalScope.launch(Dispatchers.Default) {
+    private fun jobCancel() {
+        //Understanding Jobs, how to wait for job to complete and cancel a job
+        val job: Job = GlobalScope.launch(Dispatchers.Default) {
 
             //Automatically cancels coroutine if it takes more than 3ms
             withTimeout(3000L) {
@@ -68,7 +88,31 @@ class MainActivity : AppCompatActivity() {
             job.join()      //Joins the coroutine, Main activity will wait for the coroutine
             job.cancel()    //Cancel the coroutine
         }
+    }
 
+    private fun asyncAndAwait() {
+        //By default, 2 suspend functions are executed sequentially, but for
+        //a network request, we want them to execut simultaneoursly
+
+        GlobalScope.launch(Dispatchers.IO) {
+            //measureTimeMillis() -> Meature the time taken to execute the code
+            val time = measureTimeMillis {
+                val networkCallAnswer1: Deferred<String> = async {
+                    Log.d(TAG, "Answer1: ${Thread.currentThread().name}")
+                    doNetworkCall()
+                }
+                val networkCallAnswer2 = async {
+                    Log.d(TAG, "Answer2: ${Thread.currentThread().name}")
+                    doNetworkCall2()
+                }
+
+                //await() blocks the current coroutine until networkCallAnswer1 is available
+                Log.d(TAG, "Answer1: ${networkCallAnswer1.await()}")
+                Log.d(TAG, "Answer1: ${networkCallAnswer2.await()}")
+            }
+
+            Log.d(TAG, "Time takes: $time ms")
+        }
     }
 
     private suspend fun doNetworkCall(): String {
