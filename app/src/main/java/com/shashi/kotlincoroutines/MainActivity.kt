e4a353fc351c44ad77fd4066eae93067e848063f
shashiofficial <shashiofficial10@gmail.com>
2020-11-26 12:01:06 +0530

    Added coroutine context

--- app/src/main/java/com/shashi/kotlincoroutines/MainActivity.kt
+++ app/src/main/java/com/shashi/kotlincoroutines/MainActivity.kt
@@ -8,10 +8,56 @@ import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "debug_shashi"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setContentView(R.layout.activity_main)
         super.onCreate(savedInstanceState)
 
+        //Dispatchers.Main -> For UI operations
+        //Dispatchers.IO -> For Data operations(Networking/Database Handling)
+        //Dispatchers.Default -> For Complex operations
+
+        GlobalScope.launch(Dispatchers.Main) {
+            Log.d(TAG, Thread.currentThread().name)
+
+            delay(3000L)
+
+            val networkCallAnswer1 = doNetworkCall()
+            val networkCallAnswer2 = doNetworkCall2()
+
+            Log.d(TAG, "Answer1: $networkCallAnswer1")
+            Log.d(TAG, "Answer1: $networkCallAnswer2")
+        }
+
+        //Custom thread
+        //GlobalScope.launch(newSingleThreadContext("CustomThreadName")) { }
+
+        //If you want to get data from Network and then update UI in Main thread
+        GlobalScope.launch(Dispatchers.IO) {
+            Log.d(TAG, "IO thread: ${Thread.currentThread().name}")
+
+            val answer = doNetworkCall()
+            withContext(Dispatchers.Main) {
+                //This would be executed in Main thread
+                Log.d(TAG, "Main thread: ${Thread.currentThread().name}")
+                textViewMain.text = answer
+            }
+        }
+    }
+
+    private suspend fun doNetworkCall(): String {
+        Log.d(TAG, "Network call 1: ${Thread.currentThread().name}")
+
+        delay(3000L)
+        return "Network call 1"
+    }
+
+    private suspend fun doNetworkCall2(): String {
+        Log.d(TAG, "Network call 2: ${Thread.currentThread().name}")
+
+        delay(3000L)
+        return "Network call 2"
     }
 
 }
