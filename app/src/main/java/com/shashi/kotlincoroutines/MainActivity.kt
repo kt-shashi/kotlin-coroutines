a29488e13e69e4e3430a257b42788c7fa475950f
shashiofficial <shashiofficial10@gmail.com>
2020-11-26 11:57:51 +0530

    Initial commit

---
+++ app/src/main/java/com/shashi/kotlincoroutines/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.shashi.kotlincoroutines
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
